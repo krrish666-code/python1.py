6223f8d82289fc2122028429a3c2b98f0d6621e1
Code.krrish kanojia <krrishkanojia602@gmail.com>
2025-08-29 14:58:39 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,11 @@
+name = "krrish kanojia"
+ 
+print(len(name))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.swapcase())
+print(name.startswith("krri"))
+print(name.startswith("kris"))
+print(name.endswith("nojia"))
+print(name.endswith("nojiaa"))
