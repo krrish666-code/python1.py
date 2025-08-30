25711caa18e2be465e6e79466c8cfb9c6f7caa8b
Code.krrish kanojia <krrishkanojia602@gmail.com>
2025-08-30 18:30:47 +0530

    Add files via upload

---
+++ list indexing.py
@@ -0,0 +1,12 @@
+friends = ["john","markaus","devid","alain","jennyfer"]
+print(friends[0])
+friends[0] = "krishna"
+print(friends[0])
+fruit = "apple"
+print(fruit[0:4])
+
+friends.append("krishna")# add the last word to the list use the append
+print(friends)
+l1 = [1,20,29,90,83,45]
+l1.sort()# sort is manage the number line
+print(l1)
