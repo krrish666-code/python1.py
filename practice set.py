4ccc96dee2c3398c28e616021e41e0de0ae2d881
Code.krrish kanojia <krrishkanojia602@gmail.com>
2025-09-14 00:37:47 +0530

    Add files via upload

---
+++ practice set.py
@@ -0,0 +1,19 @@
+# marks = []
+# f1 = int(input("enter marks "))
+# marks.append(f1)
+# f2 = int(input("enter marks "))
+# marks.append(f2)
+# f3 = int(input("enter marks "))
+# marks.append(f3)
+# f4 = int(input("enter marks "))
+# marks.append(f4)
+# f5 = int(input("enter marks "))
+# marks.append(f5)
+# f6 = int(input("enter marks "))
+# marks.append(f6)
+# marks.sort()
+# print(marks)
+a = 0,3,3,3,4,0,2,2,2
+m = a.count(1
+            )
+print(m)
